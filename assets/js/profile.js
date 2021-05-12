c95ee6528a707cbc1836d7abff90758600deb193
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2021-05-12 09:26:49 +0200

    Resolve strange behavior social icon with page zoom

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,5 +1,7 @@
 /* Change a string with the name of the social selected */
 function showMore(name){
+	if(window.innerWidth < 800){ return; }
+	
     var mainContent = document.getElementById("social-intro");
     mainContent.style.opacity = 0;
     window.setTimeout(function () {
